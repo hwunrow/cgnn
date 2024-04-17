74f4e946b2e94904712f1e8cd9ea0b9f2f1429b8
Han Yong Wunrow <nhw2114@columbia.edu>
2024-04-17 12:19:20 -0400

    init commit of codebook with FIPS-Borough dicts

---
+++ utils/codebook.py
@@ -0,0 +1,23 @@
+BOROUGH_FIPS_MAP = {
+    "BX": 36005,
+    "BK": 36047,
+    "MN": 36061,
+    "QN": 36081,
+    "SI": 36085,
+}
+
+FIPS_BOROUGH_MAP = {
+    36005: "BX",
+    36047: "BK",
+    36061: "MN",
+    36081: "QN",
+    36085: "SI",
+}
+
+BOROUGH_FULL_FIPS_DICT = {
+    "36005": "Bronx",
+    "36047": "Brooklyn",
+    "36061": "Manhattan",
+    "36081": "Queens",
+    "36085": "Staten Island",
+}
