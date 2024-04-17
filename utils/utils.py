a8f1d3c46750da39d3aa8d49d92f7dfd02a89f51
Han Yong Wunrow <nhw2114@columbia.edu>
2024-04-17 13:07:03 -0400

    add get_fips_list util function

--- utils/utils.py
+++ utils/utils.py
@@ -1,23 +1,14 @@
 import pandas as pd
-import pathlib
-
-BOROUGH_FIPS_MAP = {
-    "BX": 36005,
-    "BK": 36047,
-    "MN": 36061,
-    "QN": 36081,
-    "SI": 36085,
-}
+from codebook import BOROUGH_FIPS_MAP
 
 
-def getDateRange(start, end):
+def get_date_range(start, end):
     start_date = pd.to_datetime(start)
     end_date = pd.to_datetime(end)
     dates = pd.date_range(start=start_date, end=end_date, freq="D")
     return dates
 
 
-def writeListToCSV(filepath, src_list):
-    pathlib.Path(filepath).parent.mkdir(exist_ok=True)
-    save_df = pd.DataFrame(src_list)
-    save_df.to_csv(filepath, header=False, index=False)
+def get_fips_list():
+    fips_list = list(BOROUGH_FIPS_MAP.values())
+    return fips_list
