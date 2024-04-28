a530329c3d4f0007d4dfe7f9a3cfe2761c831551
Han Yong Wunrow <nhw2114@columbia.edu>
2024-04-28 13:59:47 -0400

    implement GCN, prevCase, and prevDelta models

---
+++ src/model.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch_geometric.nn import GCNConv
+import pandas as pd
+import process_data
+import numpy as np
+
+
+NODE_FEATURES = 22
+OUT_DIM = 1
+DROPOUT = 0.1
+
+
+class GCN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.MLP_embed = nn.Linear(NODE_FEATURES, 32)
+        self.conv1 = GCNConv(32, 32)
+        self.conv2 = GCNConv(64, 32)
+        self.MLP_pred = nn.Linear(64, OUT_DIM)
+
+    def forward(self, x, edge_index):
+        h = self.MLP_embed(x)
+        h = F.dropout(h, p=DROPOUT, training=self.training)
+        h0 = h
+
+        h = self.conv1(h, edge_index)
+        h = F.dropout(h, p=DROPOUT, training=self.training)
+        h = h.relu()
+        h = torch.cat((h, h0), dim=1)  # skip connection
+
+        h = self.conv2(h, edge_index)
+        h = h.relu()
+        h = F.dropout(h, p=DROPOUT, training=self.training)
+        h = torch.cat((h, h0), dim=1)  # skip connection
+
+        h = self.MLP_pred(h)
+        h = h + x[:, 1].unsqueeze(1)
+        out = h.relu()
+
+        return out
+
+
+class prevCase(nn.Module):
+    """
+    Predicts that the delta in the number of cases will be 0 (and that the actual number
+    of cases will be the same as the previous day).
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, edge_index):
+        return x[:, 1]
+
+
+class prevDelta(nn.Module):
+    """
+    Predicts that the delta in the number of cases will be the same as the delta from the
+    previous day.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, edge_index):
+        node_dict = process_data.create_node_key()
+        x = x.numpy()
+        pred_df = pd.DataFrame()
+        pred_df["key"] = np.array(list(node_dict.keys()))
+        pred_df[["fips", "date"]] = pred_df["key"].str.split("-", n=1, expand=True)
+        pred_df["date"] = pd.to_datetime(pred_df["date"])
+        pred_df["CASE_COUNT"] = x[:, 0]
+        pred_df["CASE_COUNT_7DAY_AVG"] = x[:, 1]
+
+        pred_df["CASE_DELTA"] = (
+            pred_df.groupby(["fips"])["CASE_COUNT_7DAY_AVG"].diff(-1).fillna(0)
+        )
+        pred_df["pred"] = pred_df["CASE_DELTA"] + pred_df["CASE_COUNT_7DAY_AVG"]
+        pred_df.loc[pred_df["pred"] < 0, "pred"] = 0.0
+
+        return torch.tensor(pred_df["pred"].values, dtype=torch.float32)
